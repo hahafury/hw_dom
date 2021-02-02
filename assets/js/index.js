83dc11faab002445e41a1dc046f76345c38ad266
hahafury <bladeb071@gmail.com>
2021-02-02 17:32:26 +0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+let username = document.querySelector('#username');
+let password = document.querySelector('#password');
+let registerButton = document.querySelector('#register')
+
+const USERNAME_REGEXP = /^[A-z0-9][^\s]{6,20}$/;
+const PASSWORD_REGEXP = /^(?=.*?[A-Z])(?=.*?[0-9]).{8,20}$/;
+registerButton.addEventListener('click', register);
+
+function register() {
+    let userUsername = document.querySelector('#username').value;
+    let userPassword = document.querySelector('#password').value;
+
+    if (USERNAME_REGEXP.test(userUsername) == true) {
+        username.classList.remove('invalid')
+        username.classList.add('valid');
+    } else {
+        username.classList.add('invalid')
+        username.classList.remove('valid');
+    }
+
+    if (PASSWORD_REGEXP.test(userPassword) == true) {
+        password.classList.remove('invalid')
+        password.classList.add('valid');
+    } else {
+        password.classList.add('invalid')
+        password.classList.remove('valid');
+    }
+}
